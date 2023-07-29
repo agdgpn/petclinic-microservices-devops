82bab42a343f755eb9011800bf5ad9fcadd3ddfc
Pierre Ndiaye <pierrendiaye@gmail.com>
2023-07-30 00:03:51 +0200

    Congifuration des temps attente entre les applications

--- scripts/devops/wait-for-message.sh
+++ scripts/devops/wait-for-message.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 # En attente du message passe en argumen 1 dans le container passe en argument 2
-grep -q "$1" <(docker logs --follow "$2")
+grep -q "$1" < (docker logs --follow "$2")
 echo "Message '$1'  received from '$2' on `date`"
